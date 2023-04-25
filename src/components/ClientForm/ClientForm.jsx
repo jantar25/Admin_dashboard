1fb6ea0c091f083301d10d44b71db96887d62696
unknown <glodybulambo@gmail.com>
2023-04-25 23:07:34 +0200

    add client form ongoing

--- src/components/ClientForm/ClientForm.jsx
+++ src/components/ClientForm/ClientForm.jsx
@@ -10,28 +10,43 @@ const ClientForm = ({toggleForm}) => {
     }
   return (
     <div className='form-container'>
-        <button onClick={toggleForm}>close</button>
         <form onSubmit={hanldeSubmit}>
-            <label>REMPLIS LE FORMULAIRE CI DESSOUS</label>
-            <div className="">
-                <label>INFORMATION DU CLIENT</label>
-                <div className='input-container'>
-                    <label htmlFor='name'>Nom:</label>
-                    <input name='name' type='text' placeholder='Nom' />
+            <h3>REMPLIS LE FORMULAIRE CI DESSOUS</h3>
+            <div className="divide-form">
+                <div>
+                    <h5>INFORMATION DU CLIENT</h5>
+                    <div className='input-container'>
+                        <label htmlFor='name'>Nom:</label>
+                        <input name='name' type='text' placeholder='Nom' />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='surname'>Post-Nom:</label>
+                        <input name='surname' type='text' placeholder='Post nom' />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='address'>Address:</label>
+                        <input name='address' type='text' placeholder='Address' />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='type'>Type:</label>
+                        <select name='type' placeholder='Type'>
+                            <option>Persone 1</option>
+                            <option>Persone 2</option>
+                            <option>Persone 3</option>
+                            <option>Persone 4</option>
+                        </select>
+                    </div>
                 </div>
-                <div className='input-container'>
-                    <label htmlFor='surname'>Post-Nom:</label>
-                    <input name='surname' type='text' placeholder='Post nom' />
+                <div>
+                    <h5>INFORMATION DE L'ENREGISTREUR</h5>
+                    <div className='input-container'>
+                        <label htmlFor='number'>Numero de l'agent:</label>
+                        <input name='number' type='text' placeholder="Numero de l'agent" />
+                    </div>
                 </div>
             </div>
-            <div>
-                <label>INFORMATION DE L'ENREGISTREUR</label>
-                <div className='input-container'>
-                    <label htmlFor='number'>Numero de l'agent:</label>
-                    <input name='number' type='text' placeholder="Numero de l'agent" />
-                </div>
-            </div>
-            <input type='submit' value= 'Enregistrer' />
+            <input type='submit' value='Enregistrer' />
+            <button onClick={toggleForm}>Cancel</button>
         </form>
     </div>
   )
