39a81bdf70173f7b73a2621bfa86731820d4ff23
unknown <glodybulambo@gmail.com>
2023-05-27 11:09:14 +0200

    basics are done

--- src/components/ClientForm/ClientForm.jsx
+++ src/components/ClientForm/ClientForm.jsx
@@ -14,17 +14,21 @@ const ClientForm = ({toggleForm}) => {
             <h3>REMPLISSEZ LE FORMULAIRE CI DESSOUS</h3>
             <div className="divide-form">
                 <div className='form-left'>
-                    <h5>INFORMATION DU CLIENT</h5>
+                    <h5>INFORMATION SUR L'ENTREPRISE</h5>
                     <div className='input-container'>
                         <label htmlFor='name'>Nom:</label>
                         <input name='name' type='text' placeholder='Nom' />
                     </div>
                     <div className='input-container'>
-                        <label htmlFor='surname'>Post-Nom:</label>
+                        <label htmlFor='surname'>Address:</label>
                         <input name='surname' type='text' placeholder='Post nom' />
                     </div>
                     <div className='input-container'>
-                        <label htmlFor='address'>Address:</label>
+                        <label htmlFor='address'>Numero d'impot:</label>
+                        <input name='address' type='text' placeholder='Address' />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='address'>Secteur des operations:</label>
                         <input name='address' type='text' placeholder='Address' />
                     </div>
                     <div className='input-container'>
@@ -36,12 +40,37 @@ const ClientForm = ({toggleForm}) => {
                             <option>Persone 4</option>
                         </select>
                     </div>
+                    <div className='input-container'>
+                        <label htmlFor='address'>Numero telephone:</label>
+                        <input name='address' type='text' placeholder='Address' />
+                    </div>
                 </div>
                 <div className='form-right'>
-                    <h5>INFORMATION DE L'ENREGISTREUR</h5>
+                    <h5>INFORMATION DU REPRESENTANT</h5>
                     <div className='input-container'>
-                        <label htmlFor='number'>Numero de l'agent:</label>
-                        <input name='number' type='text' placeholder="Numero de l'agent" />
+                        <label htmlFor='number'>Noms:</label>
+                        <input name='number' type='text' placeholder="Noms de l'agent" />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='number'>Enail:</label>
+                        <input name='number' type='text' placeholder="Email de l'agent" />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='type'>Type d'identite:</label>
+                        <select name='type' placeholder='Type'>
+                            <option>Passeport</option>
+                            <option>Permis de conduire</option>
+                            <option>Carte d'identite</option>
+                            <option>Autres</option>
+                        </select>
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='number'>Numero d'identite:</label>
+                        <input name='number' type='text' placeholder="Identite de l'agent" />
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor='number'>Numero de telephone:</label>
+                        <input name='number' type='text' placeholder="telephone de l'agent" />
                     </div>
                 </div>
             </div>
