55b7f3c9ce551d1048722f0a5df534b8c2d08c86
unknown <glodybulambo@gmail.com>
2023-05-30 22:14:43 +0200

    Side bar  refactoring

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import Card from './Card/Card'
-import './Cards.css'
+import Card from "./Card/Card";
+import "./Cards.css";
 
-const Cards = ({cardItems}) => {
+const Cards = ({ cardItems }) => {
   return (
-    <div className='cards-container '>
-        {cardItems.map(card => <Card key={card.id} card={card} />)}
+    <div className="cards-container">
+      {cardItems.map((card) => (
+        <Card key={card.id} card={card} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
