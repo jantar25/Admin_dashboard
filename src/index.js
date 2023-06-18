c7458d09c370d8b096ccd680c52c5165813f5519
unknown <glodybulambo@gmail.com>
2023-06-18 15:47:43 +0200

    configuring store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux'
+import {store,persistor} from './Redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store} >
+    <PersistGate loading={null} persistor={persistor} >
+      <React.Suspense fallback="Loading">
+        <App />
+      </React.Suspense>
+    </PersistGate>
+  </Provider>
 );
