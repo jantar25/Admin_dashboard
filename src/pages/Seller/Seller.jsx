e4104f8a93d72ef6e83c1fda814cb6ccc11b21bd
unknown <glodybulambo@gmail.com>
2023-06-20 21:54:54 +0200

    marchant info fetch done

--- src/pages/Seller/Seller.jsx
+++ src/pages/Seller/Seller.jsx
@@ -9,23 +9,35 @@ const Seller = () => {
   const location = useLocation()
   const currentSellerId = location.pathname.split('/')[2];
   const {isLoading,apiData,serverError} = useFetch(`${baseURL}/merchant/${currentSellerId}`)
-
+  console.log(apiData)
   return (
     <div className='container-seller'>
       {isLoading && <div className='loading'>Telechargement...</div>}
       {serverError && <div className='error'>{serverError}</div>}
       {apiData && 
       <div>
-        <div className='seller-type-container'>
-          <span className='seller-type'>{apiData.businessType}</span>
-          {/* <div className={`seller-icon-container ${apiData.status === 'active' ? 'seller-active' : 'seller-inactive'}`}>
-            {currentSeller.status}
-          </div> */}
+        <div className='seller-type-container'><span className='seller-type'>{apiData.merchantName}</span></div>
+        <div className="marchand-info-wrapper">
+          <div className='marchand-container'>
+            <h1 className='marchand-title'>INFO SUR LE MARCHAND</h1>
+            <div className='info-seller'><label>ID:</label><span>{apiData.merchantUid}</span></div>
+            <div className='info-seller'><label>Type:</label><span>{apiData.businessType}</span></div>
+            <div className='info-seller'><label>Operation:</label><span>{apiData.areaOfOperation}</span></div>
+            <div className='info-seller'><label>Nom:</label><span>{apiData.merchantRepresentativeDto.names}</span></div>
+            <div className='info-seller'><label>Taxe:</label><span>{apiData.tinNumber}</span></div>
+            <div className='info-seller'><label>Telephone:</label><span>{apiData.businessMsisdn}</span></div>
+            <div className='info-seller'><label>Address:</label><span>{apiData.address}</span></div>
+          </div>
+          <div className='marchand-container'>
+            <h1 className='marchand-title'>INFO SUR LE REPRESENTANT</h1>
+            <div className='info-seller'><label>ID:</label><span>{apiData.merchantRepresentativeDto.representativeUid}</span></div>
+            <div className='info-seller'><label>ID-Type:</label><span>{apiData.merchantRepresentativeDto.idType}</span></div>
+            <div className='info-seller'><label>ID-number:</label><span>{apiData.merchantRepresentativeDto.idNumber}</span></div>
+            <div className='info-seller'><label>Nom:</label><span>{apiData.merchantRepresentativeDto.names}</span></div>
+            <div className='info-seller'><label>Telephone:</label><span>{apiData.merchantRepresentativeDto.msisdn}</span></div>
+            <div className='info-seller'><label>Address:</label><span>{apiData.merchantRepresentativeDto.address}</span></div>
+          </div>
         </div>
-        <div className='info-seller'><label>ID:</label><span>{apiData.merchantUid}</span></div>
-        <div className='info-seller'><label>Compagny:</label><span>{apiData.merchantName}</span></div>
-        <div className='info-seller'><label>Nom:</label><span>{apiData.merchantRepresentativeDto.names}</span></div>
-        <div className='info-seller'><label>Taxe:</label><span>{apiData.tinNumber}</span></div>
       </div>
       }
     </div>
